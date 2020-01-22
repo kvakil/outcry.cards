9ee771702f3f2c7bed417c0f5656aef0dfb5993a
Keyhan Vakil <kvakil@berkeley.edu>
2020-01-22 15:48:02 -0800

    Add game logic

---
+++ lib/outcry/game.ex
@@ -0,0 +1,303 @@
+defmodule Outcry.Game do
+  defmodule Player do
+    def game_start(pid, game_pid) do
+      send(pid, %{event: "game_start", player_state: %{game_pid: game_pid}})
+    end
+
+    def state_update(pid, state) do
+      send(pid, %{event: "state_update", state: state})
+    end
+
+    def game_over(pid, score_info) do
+      send(pid, %{event: "game_over", score_info: score_info})
+    end
+
+    def place_order(pid, game_pid, order) do
+      Outcry.Game.place_order(game_pid, pid, order)
+    end
+  end
+
+  use GenServer, restart: :transient
+  alias Outcry.Game.Player
+  alias Outcry.Game.Orders
+  alias Outcry.Game.Types
+
+  @suits ~w(h j k l)a
+  @nice_ids ~w(Q W E R)
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args)
+  end
+
+  @game_length 120_000
+
+  @impl true
+  def init(args) do
+    Process.send_after(self(), :end_game, @game_length)
+    {:ok, %{}, {:continue, Map.put(args, :event, "start_game")}}
+  end
+
+  defp broadcast_to_players(f, state) do
+    state.players |> Map.keys() |> Enum.each(f)
+  end
+
+  defp clear_order_books(state) do
+    empty_order_book = %{buy: [], sell: []}
+    state |> Map.put(:order_books, Map.new(@suits, &{&1, empty_order_book}))
+  end
+
+  @distribution [8, 10, 10, 12]
+  @initial_wealth -50
+
+  @impl true
+  def handle_continue(%{event: "start_game", players: players}, state) do
+    card_distribution = Map.new(Enum.zip(@suits, Enum.shuffle(@distribution)))
+
+    {common_suit, 12} = Enum.max_by(card_distribution, fn {_suit, count} -> count end)
+    goal_suit = Types.Suit.opposite_suit(common_suit)
+
+    deck =
+      Enum.flat_map(card_distribution, fn {suit, count} -> List.duplicate(suit, count) end)
+      |> Enum.shuffle()
+
+    initial_counts = Map.new(@suits, &{&1, 0})
+
+    count_suits = fn raw_hand ->
+      Enum.reduce(raw_hand, initial_counts, fn suit, counts ->
+        Map.update!(counts, suit, &(&1 + 1))
+      end)
+    end
+
+    hands =
+      deck
+      |> Enum.chunk_every(10)
+      |> Enum.map(count_suits)
+      |> (&Enum.zip(players, &1)).()
+      |> Map.new()
+
+    handle_continue(%{event: "start_game", hands: hands, goal_suit: goal_suit}, state)
+  end
+
+  @impl true
+  def handle_continue(%{event: "start_game", hands: hands, goal_suit: goal_suit}, state) do
+    players =
+      hands
+      |> Enum.zip(@nice_ids)
+      |> Map.new(fn {{player_id, hand}, nice_id} ->
+        {player_id, %{wealth: @initial_wealth, hand: hand, nice_id: nice_id}}
+      end)
+
+    {:noreply,
+     state
+     |> Map.put(:players, players)
+     |> Map.put(:goal_suit, goal_suit)
+     |> clear_order_books(), {:continue, :broadcast_start}}
+  end
+
+  @and_then_broadcast_state {:continue, :broadcast_state}
+
+  @impl true
+  def handle_continue(:broadcast_start, state) do
+    broadcast_to_players(&Player.game_start(&1, self()), state)
+    {:noreply, state, @and_then_broadcast_state}
+  end
+
+  @impl true
+  def handle_continue(:broadcast_state, state) do
+    broadcast_to_players(&Player.state_update(&1, state), state)
+    {:noreply, state}
+  end
+
+  defp cancel_conflicting_orders(order_book_side, player) do
+    Enum.reject(order_book_side, fn {p, _} -> p == player end)
+  end
+
+  defp try_order(state, player, %Orders.Limit{suit: suit, direction: direction, price: price}) do
+    other_direction = Types.Direction.opposite_direction(direction)
+    other_side = state.order_books[suit][other_direction]
+
+    case other_side do
+      [] ->
+        :nocross
+
+      [{other_player, other_price} | _rest_of_book] ->
+        other_direction_int = Types.Direction.direction_to_int(other_direction)
+
+        if (other_price - price) * other_direction_int >= 0 do
+          if player == other_player do
+            # TODO: maybe recurse and check next order?
+            :selftrade
+          else
+            sides = %{direction => player, other_direction => other_player}
+            {:cross, {sides, other_price}}
+          end
+        else
+          :nocross
+        end
+    end
+  end
+
+  defp try_order(state, player, %Orders.Market{suit: suit, direction: direction}) do
+    fake_order = %Orders.Limit{
+      suit: suit,
+      direction: direction,
+      price: 1_000 * Types.Direction.direction_to_int(direction)
+    }
+
+    try_order(state, player, fake_order)
+  end
+
+  def place_order(server, player, order) do
+    case order.__struct__.changeset(order) do
+      {:ok, order} ->
+        GenServer.cast(server, %{
+          player: player,
+          order: order
+        })
+
+      {:error, %{errors: errors}} ->
+        {:error, errors}
+    end
+  end
+
+  defp worse?(direction_as_int, price0, price1) do
+    price0 * direction_as_int < price1 * direction_as_int
+  end
+
+  defp add_to_order_book(state, player, %Orders.Limit{
+         suit: suit,
+         direction: direction,
+         price: price
+       }) do
+    direction_as_int = Types.Direction.direction_to_int(direction)
+
+    update_in(state.order_books[suit][direction], fn order_book_half ->
+      {not_worse_than_order, worse_than_order} =
+        order_book_half
+        |> (&cancel_conflicting_orders(&1, player)).()
+        |> Enum.split_while(fn {_, other_price} ->
+          not worse?(direction_as_int, other_price, price)
+        end)
+
+      not_worse_than_order ++ [{player, price} | worse_than_order]
+    end)
+  end
+
+  defp execute_trade(state, order, {sides, cross_price} = _trade) do
+    suit = order.suit
+
+    update_wealth = fn state, {direction, player} ->
+      update_in(
+        state.players[player].wealth,
+        &(&1 - cross_price * Types.Direction.direction_to_int(direction))
+      )
+    end
+
+    update_hand = fn state, {direction, player} ->
+      update_in(
+        state.players[player].hand[suit],
+        &(&1 + Types.Direction.direction_to_int(direction))
+      )
+    end
+
+    execute = fn order_side, state ->
+      state |> update_wealth.(order_side) |> update_hand.(order_side)
+    end
+
+    _new_state = Enum.reduce(sides, state, execute) |> clear_order_books()
+  end
+
+  defp has_card?(state, player, suit) do
+    state.players[player].hand[suit] > 0
+  end
+
+  defp short_sell?(state, player, order) do
+    order.direction == :sell and not has_card?(state, player, order.suit)
+  end
+
+  @impl true
+  def handle_cast(
+        %{player: player, order: %Orders.Cancel{suit: suit, direction: direction}},
+        state
+      ) do
+    new_state =
+      update_in(state.order_books[suit][direction], &cancel_conflicting_orders(&1, player))
+
+    {:noreply, new_state, @and_then_broadcast_state}
+  end
+
+  @impl true
+  def handle_cast(%{player: player, order: order}, state) do
+    if short_sell?(state, player, order) do
+      {:noreply, state}
+    else
+      case try_order(state, player, order) do
+        :nocross ->
+          case order.__struct__ do
+            # Only limit orders can be added to the book.
+            Orders.Limit ->
+              new_state = add_to_order_book(state, player, order)
+              {:noreply, new_state, @and_then_broadcast_state}
+
+            _ ->
+              {:noreply, state}
+          end
+
+        {:cross, trade} ->
+          new_state = execute_trade(state, order, trade)
+          {:noreply, new_state, @and_then_broadcast_state}
+
+        :selftrade ->
+          {:noreply, state}
+      end
+    end
+  end
+
+  @total_goal 200
+  @points_per_goal 10
+
+  defp score(%{players: players, goal_suit: goal_suit} = _state) do
+    wealth_points = Map.new(players, fn {player_id, player} -> {player_id, player.wealth} end)
+
+    goal_points =
+      Map.new(players, fn {player_id, player} ->
+        {player_id, @points_per_goal * player.hand[goal_suit]}
+      end)
+
+    goal_points_only = Map.values(goal_points)
+    nonbonus_goal_points = Enum.sum(goal_points_only)
+
+    most_goal_points = Enum.max(goal_points_only)
+    winners = Enum.filter(goal_points, fn {_, points} -> points == most_goal_points end)
+    winners_bonus = (@total_goal - nonbonus_goal_points) / length(winners)
+
+    winners_points =
+      Map.new(winners, fn {player_id, ^most_goal_points} -> {player_id, winners_bonus} end)
+
+    sum_points = fn _, u, v -> u + v end
+
+    final_scores =
+      wealth_points
+      |> Map.merge(goal_points, sum_points)
+      |> Map.merge(winners_points, sum_points)
+
+    %{final_scores: final_scores, goal_suit: goal_suit}
+  end
+
+  defp end_game(state) do
+    score_info = score(state)
+    broadcast_to_players(&Player.game_over(&1, score_info), state)
+    state |> Map.put(:score_info, score_info)
+  end
+
+  @impl true
+  def handle_info(:end_game, state) do
+    final_state = end_game(state)
+    {:stop, :normal, final_state}
+  end
+
+  @impl true
+  def terminate(_, _state) do
+    # TODO: log result to database
+  end
+end
---
+++ test/outcry/game_test.exs
@@ -0,0 +1,355 @@
+defmodule Outcry.GameTest do
+  use ExUnit.Case, async: true
+  alias Outcry.Game.Orders.{Limit, Market, Cancel}
+  alias Outcry.Game
+  alias Game.Player
+
+  @initial_args %{
+    hands: [
+      %{h: 8, j: 0, k: 2, l: 0},
+      %{h: 0, j: 10, k: 0, l: 0},
+      %{h: 0, j: 0, k: 10, l: 0},
+      %{h: 0, j: 0, k: 0, l: 10}
+    ],
+    goal_suit: :j
+  }
+
+  defmodule FakePlayer do
+    use GenServer
+
+    def start_link(args) do
+      GenServer.start_link(__MODULE__, args)
+    end
+
+    @impl true
+    def init(%{parent_pid: _} = state) do
+      {:ok, state}
+    end
+
+    @impl true
+    def handle_info(event, %{parent_pid: parent_pid} = state) do
+      case event do
+        %{event: "state_update", state: player_state} ->
+          {:noreply, state |> Map.put(:player_state, player_state)}
+
+        _ ->
+          send(parent_pid, {self(), event})
+          {:noreply, state}
+      end
+    end
+
+    @impl true
+    def handle_call(:state, _from, state) do
+      {:reply, Map.get(state, :player_state), state}
+    end
+
+    def get_state(player) do
+      GenServer.call(player, :state)
+    end
+  end
+
+  setup do
+    players =
+      Enum.map(1..4, fn id ->
+        start_supervised!({FakePlayer, %{parent_pid: self()}}, id: id)
+      end)
+
+    args = update_in(@initial_args.hands, &Map.new(Enum.zip(players, &1)))
+    game = start_supervised!({Outcry.Game, args})
+
+    %{game: game, players: players, args: args}
+  end
+
+  test "game starts", %{game: game, players: players} do
+    Enum.each(players, fn player ->
+      assert_receive {^player, %{event: "game_start", player_state: %{game_pid: ^game}}}
+    end)
+  end
+
+  @extra_wait_time 100
+
+  defmacro assert_player_state(player, state) do
+    quote do
+      player = unquote(player)
+      Process.sleep(@extra_wait_time)
+      assert %{players: %{^player => unquote(state)}} = FakePlayer.get_state(player)
+    end
+  end
+
+  test "simple trade works", %{game: game, players: players} do
+    [player_h, player_j | _] = players
+    sell_order = %Limit{suit: :h, direction: :sell, price: 5}
+
+    assert_player_state(player_h, %{hand: %{h: 8}})
+    assert_player_state(player_j, %{hand: %{h: 0}})
+    assert :ok = Player.place_order(player_h, game, sell_order)
+    assert :ok = Player.place_order(player_j, game, %{sell_order | direction: :buy})
+    assert_player_state(player_h, %{hand: %{h: 7}, wealth: -45})
+    assert_player_state(player_j, %{hand: %{h: 1}, wealth: -55})
+  end
+
+  test "trade uses standing order price", %{game: game, players: players} do
+    [player_h, player_j | _] = players
+    sell_order = %Limit{suit: :h, direction: :sell, price: 5}
+    buy_order = %Limit{suit: :h, direction: :buy, price: 10}
+
+    assert_player_state(player_h, %{hand: %{h: 8}})
+    assert_player_state(player_j, %{hand: %{h: 0}})
+    assert :ok = Player.place_order(player_h, game, sell_order)
+    assert :ok = Player.place_order(player_j, game, buy_order)
+    assert_player_state(player_h, %{hand: %{h: 7}, wealth: -45})
+    assert_player_state(player_j, %{hand: %{h: 1}, wealth: -55})
+  end
+
+  test "trade uses best price", %{game: game, players: players} do
+    [player_h, player_j, player_k | _] = players
+    sell_order = %Limit{suit: :k, direction: :sell, price: 5}
+
+    assert_player_state(player_h, %{hand: %{k: 2}})
+    assert_player_state(player_j, %{hand: %{k: 0}})
+    assert_player_state(player_k, %{hand: %{k: 10}})
+    assert :ok = Player.place_order(player_h, game, sell_order)
+    assert :ok = Player.place_order(player_k, game, %{sell_order | price: 4})
+    assert :ok = Player.place_order(player_j, game, %{sell_order | direction: :buy})
+    assert_player_state(player_h, %{hand: %{k: 2}, wealth: -50})
+    assert_player_state(player_j, %{hand: %{k: 1}, wealth: -54})
+    assert_player_state(player_k, %{hand: %{k: 9}, wealth: -46})
+  end
+
+  test "trade uses first time when price conflicts", %{game: game, players: players} do
+    [player_h, player_j, player_k | _] = players
+    sell_order = %Limit{suit: :k, direction: :sell, price: 5}
+
+    assert_player_state(player_h, %{hand: %{k: 2}})
+    assert_player_state(player_j, %{hand: %{k: 0}})
+    assert_player_state(player_k, %{hand: %{k: 10}})
+    assert :ok = Player.place_order(player_h, game, sell_order)
+    assert :ok = Player.place_order(player_k, game, sell_order)
+    assert :ok = Player.place_order(player_j, game, %{sell_order | direction: :buy})
+    assert_player_state(player_h, %{hand: %{k: 1}, wealth: -45})
+    assert_player_state(player_j, %{hand: %{k: 1}, wealth: -55})
+    assert_player_state(player_k, %{hand: %{k: 10}, wealth: -50})
+  end
+
+  test "market order works", %{game: game, players: players} do
+    [player_h, player_j | _] = players
+    market_order = %Market{suit: :k, direction: :sell}
+    buy_order = %Limit{suit: :k, direction: :buy, price: 200}
+
+    assert_player_state(player_h, %{hand: %{k: 2}})
+    assert_player_state(player_j, %{hand: %{k: 0}})
+    assert :ok = Player.place_order(player_j, game, buy_order)
+    assert :ok = Player.place_order(player_h, game, market_order)
+    assert_player_state(player_h, %{hand: %{k: 1}, wealth: 150})
+    assert_player_state(player_j, %{hand: %{k: 1}, wealth: -250})
+  end
+
+  test "market order selects best price", %{game: game, players: players} do
+    [player_h, player_j, player_k | _] = players
+    sell_order = %Limit{suit: :k, direction: :sell, price: 5}
+    market_order = %Market{suit: :k, direction: :buy}
+
+    assert_player_state(player_h, %{hand: %{k: 2}})
+    assert_player_state(player_j, %{hand: %{k: 0}})
+    assert_player_state(player_k, %{hand: %{k: 10}})
+    assert :ok = Player.place_order(player_h, game, sell_order)
+    # next order is better and should get filled
+    assert :ok = Player.place_order(player_k, game, %{sell_order | price: 4})
+    assert :ok = Player.place_order(player_j, game, market_order)
+    assert_player_state(player_h, %{hand: %{k: 2}, wealth: -50})
+    assert_player_state(player_j, %{hand: %{k: 1}, wealth: -54})
+    assert_player_state(player_k, %{hand: %{k: 9}, wealth: -46})
+  end
+
+  test "failed market order not on books", %{game: game, players: players} do
+    [player_h, player_j | _] = players
+    market_order = %Market{suit: :k, direction: :sell}
+    buy_order = %Limit{suit: :k, direction: :buy, price: 200}
+
+    assert_player_state(player_h, %{hand: %{k: 2}})
+    assert_player_state(player_j, %{hand: %{k: 0}})
+    # next order killed
+    assert :ok = Player.place_order(player_h, game, market_order)
+    # next order does not hit market order above
+    assert :ok = Player.place_order(player_j, game, buy_order)
+    assert_player_state(player_h, %{hand: %{k: 2}, wealth: -50})
+    assert_player_state(player_j, %{hand: %{k: 0}, wealth: -50})
+  end
+
+  test "cancel order explicitly", %{game: game, players: players} do
+    [player_h, player_j | _] = players
+    sell_order = %Limit{suit: :h, direction: :sell, price: 5}
+
+    assert_player_state(player_h, %{hand: %{h: 8}})
+    assert_player_state(player_j, %{hand: %{h: 0}})
+    assert :ok = Player.place_order(player_h, game, sell_order)
+    assert :ok = Player.place_order(player_h, game, %Cancel{suit: :h, direction: :sell})
+    assert :ok = Player.place_order(player_j, game, %{sell_order | direction: :buy})
+    assert_player_state(player_h, %{hand: %{h: 8}})
+    assert_player_state(player_j, %{hand: %{h: 0}})
+  end
+
+  test "conflicting order cancels existing", %{game: game, players: players} do
+    [player_h, player_j | _] = players
+    sell_order = %Limit{suit: :h, direction: :sell}
+    buy_order = %{sell_order | direction: :buy}
+
+    assert_player_state(player_h, %{hand: %{h: 8}})
+    assert_player_state(player_j, %{hand: %{h: 0}})
+    assert :ok = Player.place_order(player_h, game, %{sell_order | price: 5})
+    assert :ok = Player.place_order(player_h, game, %{sell_order | price: 6})
+    assert :ok = Player.place_order(player_j, game, %{buy_order | price: 5})
+    assert_player_state(player_h, %{hand: %{h: 8}})
+    assert_player_state(player_j, %{hand: %{h: 0}})
+    assert :ok = Player.place_order(player_j, game, %{buy_order | price: 6})
+    assert_player_state(player_h, %{hand: %{h: 7}})
+    assert_player_state(player_j, %{hand: %{h: 1}})
+  end
+
+  test "trade clears all suits", %{game: game, players: players} do
+    [player_h, player_j | _] = players
+    sell_order_h = %Limit{suit: :h, direction: :sell, price: 5}
+    sell_order_k = %{sell_order_h | suit: :k}
+
+    assert_player_state(player_h, %{hand: %{h: 8}})
+    assert_player_state(player_j, %{hand: %{h: 0}})
+    assert_player_state(player_h, %{hand: %{k: 2}})
+    assert_player_state(player_j, %{hand: %{k: 0}})
+    assert :ok = Player.place_order(player_h, game, sell_order_h)
+    # next order will be cleared by trade in h suit
+    assert :ok = Player.place_order(player_h, game, sell_order_k)
+    assert :ok = Player.place_order(player_j, game, %{sell_order_h | direction: :buy})
+    assert_player_state(player_h, %{hand: %{h: 7}, wealth: -45})
+    assert_player_state(player_j, %{hand: %{h: 1}, wealth: -55})
+    assert :ok = Player.place_order(player_j, game, %{sell_order_k | direction: :buy})
+    # trade in k suit does not happen
+    assert_player_state(player_h, %{hand: %{k: 2}, wealth: -45})
+    assert_player_state(player_j, %{hand: %{k: 0}, wealth: -55})
+  end
+
+  test "short sell denied", %{game: game, players: players} do
+    [player_h, player_j | _] = players
+    sell_order = %Limit{suit: :j, direction: :sell, price: 5}
+
+    assert_player_state(player_h, %{hand: %{j: 0}})
+    # next order denied
+    assert :ok = Player.place_order(player_h, game, sell_order)
+    # next order does not hit denied order above
+    assert :ok = Player.place_order(player_j, game, %{sell_order | direction: :buy})
+    assert_player_state(player_h, %{hand: %{j: 0}, wealth: -50})
+  end
+
+  test "blantantly invalid orders denied", %{game: game, players: players} do
+    [player_h | _] = players
+    too_large_order = %Limit{suit: :h, direction: :buy, price: +1_000_000}
+    too_small_order = %Limit{suit: :h, direction: :buy, price: -1_000_000}
+
+    Enum.each(
+      [%Limit{}, %Market{}, %Cancel{}, too_large_order, too_small_order],
+      fn bad_order ->
+        assert {:error, _} = Player.place_order(player_h, game, bad_order)
+      end
+    )
+  end
+
+  test "basic end game scores", %{game: game, players: players} do
+    player_j = Enum.fetch!(players, 1)
+
+    final_scores =
+      Map.new(players, fn player ->
+        {player, if(player == player_j, do: 150.0, else: -50)}
+      end)
+
+    send(game, :end_game)
+
+    Enum.each(players, fn player ->
+      assert_receive {^player,
+                      %{
+                        event: "game_over",
+                        score_info: %{final_scores: ^final_scores, goal_suit: :j}
+                      }}
+    end)
+  end
+
+  test "end game includes wealth", %{game: game, players: players} do
+    final_scores = Map.new(Enum.zip(players, [-45, 145.0, -50, -50]))
+
+    [player_h, player_j | _] = players
+    sell_order = %Limit{suit: :h, direction: :sell, price: 5}
+
+    assert_player_state(player_h, %{hand: %{h: 8}})
+    assert_player_state(player_j, %{hand: %{h: 0}})
+    assert :ok = Player.place_order(player_h, game, sell_order)
+    assert :ok = Player.place_order(player_j, game, %{sell_order | direction: :buy})
+    assert_player_state(player_h, %{hand: %{h: 7}, wealth: -45})
+    assert_player_state(player_j, %{hand: %{h: 1}, wealth: -55})
+
+    send(game, :end_game)
+
+    Enum.each(players, fn player ->
+      assert_receive {^player,
+                      %{
+                        event: "game_over",
+                        score_info: %{final_scores: ^final_scores, goal_suit: :j}
+                      }}
+    end)
+  end
+
+  test "end game users tie", %{game: game, players: players} do
+    final_scores = Map.new(Enum.zip(players, [45.0, 55.0, -50, -50]))
+
+    [player_h, player_j | _] = players
+    buy_order = %Limit{suit: :j, direction: :buy, price: 1}
+
+    Enum.each(1..5, fn _ ->
+      assert :ok = Player.place_order(player_h, game, buy_order)
+      assert :ok = Player.place_order(player_j, game, %{buy_order | direction: :sell})
+    end)
+
+    send(game, :end_game)
+
+    Enum.each(players, fn player ->
+      assert_receive {^player,
+                      %{
+                        event: "game_over",
+                        score_info: %{final_scores: ^final_scores, goal_suit: :j}
+                      }}
+    end)
+  end
+
+  test "fuzz game state", %{game: game, players: players} do
+    Enum.each(1..10000, fn _ ->
+      type = Enum.random([Limit, Market, Cancel])
+      player = Enum.random(players)
+      suit = Enum.random(~w(h j k l)a)
+      direction = Enum.random(~w(buy sell)a)
+
+      order =
+        case type do
+          Limit ->
+            price = Enum.random(0..200)
+            %Limit{suit: suit, direction: direction, price: price}
+
+          Market ->
+            %Market{suit: suit, direction: direction}
+
+          Cancel ->
+            %Cancel{suit: suit, direction: direction}
+        end
+
+      assert :ok = Player.place_order(player, game, order)
+    end)
+
+    send(game, :end_game)
+
+    Enum.each(players, fn player ->
+      assert_receive {^player,
+                      %{
+                        event: "game_over",
+                        score_info: %{final_scores: final_scores, goal_suit: :j}
+                      }}
+
+      assert Enum.sum(Map.values(final_scores)) == 0
+    end)
+  end
+end
