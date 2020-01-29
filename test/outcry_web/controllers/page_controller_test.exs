6717fa97bb557bcf35ff85dbd7da5e843a8d1db2
Keyhan Vakil <kvakil@berkeley.edu>
2020-01-28 21:33:46 -0800

    Fix failing test

--- test/outcry_web/controllers/page_controller_test.exs
+++ test/outcry_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule OutcryWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Outcry"
   end
 end
