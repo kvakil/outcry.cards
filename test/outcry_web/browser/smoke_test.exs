7441f286f2a6d78be20934f03e07fa949dc75ad0
Keyhan Vakil <kvakil@berkeley.edu>
2020-03-17 20:44:13 -0700

    Improve test code

--- test/outcry_web/browser/smoke_test.exs
+++ test/outcry_web/browser/smoke_test.exs
@@ -4,7 +4,6 @@ defmodule OutcryWeb.Browser.SmokeTest do
   alias Wallaby.{Element, Query}
 
   @register_button Query.link("Register", count: 2) |> Query.at(0)
-
   test "registration link works", %{session: session} do
     assert session
            |> visit("/")
@@ -22,17 +21,23 @@ defmodule OutcryWeb.Browser.SmokeTest do
            |> String.ends_with?("/play")
   end
 
+  @looking_for_game Query.css(".title", text: "Looking for game...")
   test "matchmaking works" do
-    users = Enum.map(1..4, fn i ->
-      {:ok, user} = Wallaby.start_session()
-      user
-      |> visit("/play")
-      |> assert_has(Query.css(".title"))
-      |> assert_text("Looking for game...")
-      user
-    end)
+    users =
+      Stream.repeatedly(fn ->
+        {:ok, user} = Wallaby.start_session()
+
+        user
+        |> visit("/play")
+        |> assert_has(@looking_for_game)
+
+        user
+      end)
+      |> Enum.take(4)
 
     Process.sleep(1000)
-    users |> List.first() |> refute_has(Query.css(".title"))
+    Enum.each(users, fn user ->
+      user |> refute_has(@looking_for_game)
+    end)
   end
 end
